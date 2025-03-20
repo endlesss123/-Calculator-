b0a97f26afae228bf9bb8770bb7162f24e4beb5d
Endless Thao <endlessthao3@gmail.com>
2025-03-19 19:01:20 -0700

    Add initial Calculator code

---
+++ src/main/java/com/github/endlesss123/calculator_wn25/Main.java
@@ -0,0 +1,12 @@
+package com.github.endlesss123.calculator_wn25;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(5, 3);
+        System.out.println("5 + 3 = " + sum);
+
+    }
+}
+
